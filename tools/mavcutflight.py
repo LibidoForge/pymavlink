e6da47e448b9247f20377a50f72da55225733a76
LibidoForge <LibidoForge@users.noreply.github.com>
2023-06-22 04:03:31 +0300

    Update mavcutflight.py

--- tools/mavcutflight.py
+++ tools/mavcutflight.py
@@ -7,6 +7,7 @@ from __future__ import print_function
 
 import os
 import struct
+import re
 
 from argparse import ArgumentParser
 parser = ArgumentParser(description=__doc__)
@@ -38,8 +39,10 @@ def process(filename):
     file_header = bytearray()
 
     messages = []
-    in_air = False
+    takeoff = False
     start_time = 0.0
+    last_curr_up = 0
+    last_curr = 0
 
     # dictionary of outputs by sysid
     output = {}
@@ -52,7 +55,43 @@ def process(filename):
         if args.condition and not mavutil.evaluate_condition(args.condition, mlog.messages):
             continue
 
-        if args.condition and not mavutil.evaluate_condition(args.condition, mlog.messages):
+        m = mlog.recv_match(type=['SYS_STATUS','STATUSTEXT'], condition=args.condition)
+        if groundspeed > args.groundspeed and not in_air:
+            print("In air at %s (percent %.0f%% groundspeed %.1f)" % (time.asctime(t), mlog.percent, groundspeed))
+            in_air = True
+            start_time = time.mktime(t)
+        elif groundspeed < args.groundspeed and in_air:
+            print("On ground at %s (percent %.1f%% groundspeed %.1f  time=%.1f seconds)" % (
+                time.asctime(t), mlog.percent, groundspeed, time.mktime(t) - start_time))
+            in_air = False
+            total_time += time.mktime(t) - start_time
+        
+        # power consumption
+        if takeoff == 0 and m.get_type() == "SYS_STATUS":
+            if int(m.current_battery)*0.01 > 10: last_curr_up += 1
+            else: last_curr_up = 0
+                
+        # High power consumption for 5 sec, presume takeoff
+        if takeoff == 0 and last_curr_up >= 5:
+            last_curr_up = 0
+            takeoff = 1
+            
+        # Takeoff message
+        if takeoff == 0 and m.get_type() == "STATUSTEXT" and re.match(r".*takeoff.*", str(m.Text), re.I):
+            takeoff = 1
+
+        # Skip pre
+        if takeoff == 0: continue
+            
+        # power consumption
+        if takeoff == 1 and m.get_type() == "SYS_STATUS":
+            if int(m.current_battery)*0.01 < 5: last_curr += 1
+            else: last_curr = 0
+                
+        # No power consumption for 5 sec, presume landing
+        if takeoff == 1 and last_curr >= 5:				
+            last_curr = 0
+            takeoff = 0
             continue
 
         sysid = m.get_srcSystem()
